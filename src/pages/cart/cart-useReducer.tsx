c1d3ee4bd4de9136592a846ebc3de23cc104ad00
Summer <h.yy@ngarihealth.com>
2021-07-09 16:56:30 +0800

    自定义hooks

--- src/pages/cart/cart-useReducer.tsx
+++ src/pages/cart/cart-useReducer.tsx
@@ -1,20 +1,20 @@
 /*
- * @Description  : 购物车-使用useReducer
+ * @Description  : 购物车
  * @Author       : huyanyan
  * @Date         : 2021-07-06 15:08:13
  */
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import CartList from "../../components/list";
 import { ItemProps, ChoosePProps } from "../../components/listType";
 import { CartContext } from "./cartConext";
-// import { useMethodReducer } from "../../components/useMethodReducer";
+
 import "../../assets/main.css";
 
 const Cart = () => {
   /**
    * 列表数据
    */
-  const [list, setList] = useState<Array<ItemProps>>([
+  let [list, setList] = useState<Array<ItemProps>>([
     {
       name: "西瓜",
       price: 12,
@@ -34,33 +34,52 @@ const Cart = () => {
   /**
    * 总金额
    */
-  const [totalPrice, setTotalPrice] = useState(0);
+  let [totalPrice, setTotalPrice] = useState(0);
   /**
    * 全选状态 false:未选中  true：选中
    */
-  const [checkedAll, setcheckedAll] = useState(false);
+  let [checkedAll, setcheckedAll] = useState(false);
   /**
    * 全选事件
    */
-  const selectAll = (event: ChoosePProps) => {
-    setcheckedAll(event.target.checked);
+  const selectAll = () => {
+    checkedAll = !checkedAll;
+    setcheckedAll(checkedAll);
+    checkAll();
+  };
+  /**
+   * 全选操作
+   */
+  const checkAll = () => {
+    // 金额初始化
+    totalPrice = 0;
+    // 状态变化
+    list.forEach((item) => {
+      item.isChecked = checkedAll;
+      if (checkedAll) {
+        totalPrice += item.price;
+      }
+    });
+    setTotalPrice(totalPrice);
+    setList([...list]);
   };
 
   return (
     <div>
       <CartContext.Provider value={{ setcheckedAll, setList, setTotalPrice }}>
         <CartList checkedAll={checkedAll} list={list} totalPrice={totalPrice} />
-        <div className="cart-list">
-          <input
-            type="checkbox"
-            checked={checkedAll}
-            onChange={(event) => selectAll(event)}
-          />
-          <span className="cart-name">全选</span>
-          <span className="cart-name">总金额： {totalPrice}</span>
-          <div className="cart-count">结算</div>
-        </div>
       </CartContext.Provider>
+
+      <div className="cart-list">
+        <input
+          type="checkbox"
+          checked={checkedAll}
+          onChange={() => selectAll()}
+        />
+        <span className="cart-name">全选</span>
+        <span className="cart-name">总金额： {totalPrice}</span>
+        <div className="cart-count">结算</div>
+      </div>
     </div>
   );
 };
